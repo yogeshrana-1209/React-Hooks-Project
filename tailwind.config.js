25c9299cdc2787a084465088408fea1679ce8a49
Yogesh Rana <yr70416@gmail.com>
2024-11-11 00:54:26 +0530

    first deploy react-hooks

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,22 +9,3 @@ export default {
   },
   plugins: [],
 }
-
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  theme: {
-    extend: {
-      animation: {
-        rotate: "rotate 10s linear infinite",
-      },
-      keyframes: {
-        rotate: {
-          "0%": { transform: "rotate(0deg) scale(10)" },
-          "100%": { transform: "rotate(-360deg) scale(10)" },
-        },
-      },
-    },
-  },
-  plugins: [],
-};
-
